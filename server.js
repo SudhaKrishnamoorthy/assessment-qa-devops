5a7b6512fd5b8ac6ebfd321316443f7eb3131bb4
Sudha Krishnamoorthy <sudhavkr@hotmail.com>
2022-11-05 12:00:26 -0500

    assessment-qa-devops

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '../public')))
 
-//app.get("/", function(req, res) {
- //   res.sendFile(path.join(__dirname, "../public/index.html"))
-//})
-
-
 app.get('/api/robots', (req, res) => {
     try {
         res.status(200).send(botsArr)
